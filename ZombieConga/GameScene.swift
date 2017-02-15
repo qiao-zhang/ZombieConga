2b9f9cb349505f4d900b683872cb6508a6a2cb03
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-15 12:17:35 -0500

    restrict playable area

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,12 +11,26 @@ import UIKit
 
 class GameScene: SKScene {
   
+  let playableRect: CGRect
   let zombie = SKSpriteNode(imageNamed: "zombie1")
   var lastUpdateTime: TimeInterval = 0
   var dt: TimeInterval = 0
   let zombieMovePointsPerSec: CGFloat = 480.0
   var direction = CGPoint.zero
+
+  override init(size: CGSize) {
+    let maxWidthHeightRatio: CGFloat = 16.0 / 9.0
+    let playableHeight = size.width / maxWidthHeightRatio
+    let playableMargin = (size.height - playableHeight) / 2.0
+    playableRect = CGRect(x: 0, y: playableMargin,
+                          width: size.width, height: playableHeight)
+    super.init(size: size)
+  }
   
+  required init(coder aDecoder: NSCoder) {
+    fatalError("`init(coder:)` has not been implemented")
+  }
+
   override func didMove(to view: SKView) {
 //    super.didMove(to: view)
     backgroundColor = SKColor.black
@@ -27,8 +41,9 @@ class GameScene: SKScene {
     
     // add the zombie
     zombie.position = CGPoint(x: 400, y: 400)
-//    zombie.setScale(2)
     addChild(zombie)
+    
+    debugDrawPlayableArea()
   }
 
   override func update(_ currentTime: TimeInterval) {
@@ -42,8 +57,24 @@ class GameScene: SKScene {
     let velocity = CGPoint(x: direction.x * zombieMovePointsPerSec,
                         y: direction.y * zombieMovePointsPerSec)
     move(sprite: zombie, velocity: velocity)
+    boundsCheckZombie()
   }
-
+  
+  override func touchesBegan(_ touches: Set<UITouch>,
+                             with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let touchLocation = touch.location(in: self)
+    sceneTouched(touchLocation: touchLocation)
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>,
+                             with event: UIEvent!) {
+    guard let touch = touches.first else { return }
+    let touchLocation = touch.location(in: self)
+    sceneTouched(touchLocation: touchLocation)
+  }
+  
+  // Helper functions
   private func move(sprite: SKSpriteNode, velocity: CGPoint) {
     let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
                                y: velocity.y * CGFloat(dt))
@@ -60,21 +91,39 @@ class GameScene: SKScene {
                             y: offset.y / CGFloat(length))
   }
   
-  func sceneTouched(touchLocation: CGPoint) {
+  private func sceneTouched(touchLocation: CGPoint) {
     setZombieDirection(towards: touchLocation)
   }
   
-  override func touchesBegan(_ touches: Set<UITouch>,
-                             with event: UIEvent?) {
-    guard let touch = touches.first else { return }
-    let touchLocation = touch.location(in: self)
-    sceneTouched(touchLocation: touchLocation)
+  private func boundsCheckZombie() {
+    let bottomLeft = CGPoint(x: 0, y: playableRect.minY)
+    let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+    
+    if zombie.position.x <= bottomLeft.x {
+      zombie.position.x = bottomLeft.x
+      direction.x = -direction.x
+    }
+    if zombie.position.x >= topRight.x {
+      zombie.position.x = topRight.x
+      direction.x = -direction.x
+    }
+    if zombie.position.y <= bottomLeft.y {
+      zombie.position.y = bottomLeft.y
+      direction.y = -direction.y
+    }
+    if zombie.position.y >= topRight.y {
+      zombie.position.y = topRight.y
+      direction.y = -direction.y
+    }
   }
   
-  override func touchesMoved(_ touches: Set<UITouch>,
-                             with event: UIEvent!) {
-    guard let touch = touches.first else { return }
-    let touchLocation = touch.location(in: self)
-    sceneTouched(touchLocation: touchLocation)
+  private func debugDrawPlayableArea() {
+    let shape = SKShapeNode()
+    let path = CGMutablePath()
+    path.addRect(playableRect)
+    shape.path = path
+    shape.strokeColor = SKColor.red
+    shape.lineWidth = 4.0
+    addChild(shape)
   }
 }
