73b59e7ea86d3a95471e0cfcc3e1191b5ebaef41
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-19 14:30:43 -0500

    finish AvailableFonts

--- AvailableFonts/GameScene.swift
+++ AvailableFonts/GameScene.swift
@@ -7,83 +7,55 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+  var familyIndex = -1
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override init(size: CGSize) {
+    super.init(size: size)
+    showNextFamily()
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>,
+                             with event: UIEvent?) {
+    showNextFamily()
+  }
+
+  private func showCurrentFamily() -> Bool {
+    removeAllChildren()
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    let familyName = UIFont.familyNames[familyIndex]
+    let fontNames = UIFont.fontNames(forFamilyName: familyName)
+    if fontNames.count == 0 {
+      return false
     }
+    print("Family: \(familyName)")
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    for (idx, fontName) in fontNames.enumerated() {
+      let label = SKLabelNode(fontNamed: fontName)
+      label.text = fontName
+      label.position = CGPoint(
+        x: size.width/2,
+        y: (size.height * (CGFloat(idx+1))) / (CGFloat(fontNames.count)+1))
+      label.fontSize = 50
+      label.verticalAlignmentMode = .center
+      addChild(label)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    return true
+  }
+  
+  func showNextFamily() {
+    var familyShown = false
+    repeat {
+      familyIndex += 1
+      if familyIndex >= UIFont.familyNames.count {
+        familyIndex = 0
+      }
+      familyShown = showCurrentFamily()
+    } while !familyShown
+  }
 }
